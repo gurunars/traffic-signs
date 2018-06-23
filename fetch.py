7d972069be3f4fca9a8db402a9e3447bc1012705
Anton Berezin <gurunars@gmail.com>
2018-06-23 22:23:04 +0300

    ff

--- fetch.py
+++ fetch.py
@@ -1,2 +1,33 @@
-page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-tree = html.fromstring(page.content)
+from lxml import html
+import requests
+
+
+PATTERN = "https://www.flickr.com/photos/liikennevirasto/albums/"
+DOMAIN = "https://www.liikennevirasto.fi"
+
+
+def fetch(path):
+    page = requests.get(DOMAIN + path)
+    return html.fromstring(page.content)
+
+
+"""
+tree = fetch("/web/en/road-network/traffic-signs")
+
+sections = tree.xpath('//div[@class="section-content"]')
+
+urls = []
+
+for section in sections:
+    urls += section.xpath('*/a/@href')
+    urls += section.xpath('a/@href')
+
+hrefs = []
+for url in urls:
+    print("Proc: " + url)
+    page = fetch(url)
+    hrefs += [
+        href for href in page.xpath("//a/@href") if href.startswith(PATTERN)
+    ]
+
+"""
